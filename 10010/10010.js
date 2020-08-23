93dedadf3456af39492bb37a72e649830bba7fea
GideonSenku <gentlewind2391@gmail.com>
2020-08-23 21:09:30 +0800

    添加[10010]脚本

---
+++ 10010/10010.js
@@ -0,0 +1,81 @@
+const tel = `填入你的电话号码`
+const VAL_loginheader = `填入来自BoxJs的数据`
+
+const $ = new importModule('Env')()
+const res = await getinfo()
+if (config.runsInWidget) {
+  let widget = createWidget(res)
+  Script.setWidget(widget)
+  Script.complete()
+}
+
+function createWidget(res) {
+  const signinfo = res
+  if (signinfo.code == 'Y') {
+    // 基本信息
+
+    const traffic = signinfo.data.dataList[0]
+    const flow = signinfo.data.dataList[1]
+    const voice = signinfo.data.dataList[2]
+    const credit = signinfo.data.dataList[3]
+    const back = signinfo.data.dataList[4]
+    const money = signinfo.data.dataList[5]
+
+
+    const w = new ListWidget()
+    const bgColor = new LinearGradient()
+    bgColor.colors = [new Color("#1c1c1c"), new Color("#29323c")]
+    bgColor.locations = [0.0, 1.0]
+    w.backgroundGradient = bgColor
+    w.centerAlignContent()
+
+    const time = new Date()
+    const dfTime = new DateFormatter()
+    dfTime.locale = "en"
+    dfTime.useMediumDateStyle()
+    dfTime.useNoTimeStyle()
+
+    const firstLine = w.addText(`[📱]中国联不通`)
+    firstLine.textSize = 12
+    firstLine.textColor = Color.white()
+    firstLine.textOpacity = 0.7
+
+    const trafficLine = w.addText(`[${traffic.remainTitle}]${traffic.number}${traffic.unit}`)
+    trafficLine.textSize = 12
+    trafficLine.textColor = Color.white()
+
+    const flowLine = w.addText(`[${flow.remainTitle}]${flow.number}${flow.unit}`)
+    flowLine.textSize = 12
+    flowLine.textColor = new Color("#6ef2ae")
+
+    const voiceLine = w.addText(`[${voice.remainTitle}]${voice.number}${voice.unit}`)
+    voiceLine.textSize = 12
+    voiceLine.textColor = new Color("#7dbbae")
+
+    const creditLine = w.addText(`[${credit.remainTitle}]${credit.number}${credit.unit}`)
+    creditLine.textSize = 12
+    creditLine.textColor = new Color("#ff9468")
+
+    const backLine = w.addText(`[${back.remainTitle}]${back.number}${back.unit}`)
+    backLine.textSize = 12
+    backLine.textColor = new Color("#ffcc66")
+
+    const moneyLine = w.addText(`[${money.remainTitle}]${money.number}${money.unit}`)
+    moneyLine.textSize = 12
+    moneyLine.textColor = new Color("#ffa7d3")
+    return w
+  }
+}
+
+
+async function getinfo() {
+  const url = {
+    url: `https://m.client.10010.com/mobileService/home/queryUserInfoSeven.htm?version=iphone_c@7.0403&desmobiel=${tel}&showType=3`,
+    headers: {
+      "Cookie": JSON.parse(VAL_loginheader)["Cookie"]
+    }
+  }
+  const res = await $.get(url)
+  log(res)
+  return res
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Scriptable
+
+
+## China Unicom Info
+
+> 2020.08.23 第一次实现联通信息脚本
+
+### 食用方式
+0. 前提你需要一个`Scriptable` [TestFlight地址](https://testflight.apple.com/join/uN1vTqxk)
+1. 文件问题(任意选一个作为方案)
+   - 下载本仓库的`Env.js`和`10010.js`保存到`Scriptable`(记得将脚本命名为Env和10010)
+   - 下载本仓库的`Env.scriptable`和`10010.scriptable`直接导入到`Scriptable`
+   - 以上两种区别在于图标和文件名是否需要手动重命名
+2. 修改`10010`脚本的个人数据
+   - tel: 你的联通电话号码
+   - VAL_loginheader: 来自BoxJs的数据`chavy_tokenheader_10010`
+
+3. 小组件样式参考[@spencerwooo](https://gist.github.com/spencerwooo/7955aefc4ffa5bc8ae7c83d85d05e7a4)
+4. 添加小组件并选取`10010`脚本即可
